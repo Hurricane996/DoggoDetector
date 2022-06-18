9ba7fc42b3a83151f28ef454f6a39d696ed12ded
Hurricane996 <levijwillrich@gmail.com>
2022-06-17 22:14:27 -0500

    git is fucked already, trying to fix it

---
+++ src/cv.rs
@@ -0,0 +1,42 @@
+use std::{time::{Duration, SystemTime}, sync::Arc};
+
+use tokio::{sync::RwLock, time::sleep};
+
+use crate::DogSighting;
+
+
+const CV_LOOP_SLEEP_TIME: Duration = Duration::from_secs(2);
+
+pub fn setup_cv_loop(last_sighting: Arc<RwLock<Option<DogSighting>>>) {
+    let cv_subsystem = CVSubsystem::new();
+
+
+    let last_sighting = last_sighting.clone();
+    tokio::spawn(async move {
+        loop {
+            if let Some(image) = cv_subsystem.get_dog() {
+                let sighting = DogSighting {
+                    image,
+                    timestamp: SystemTime::now()
+                };
+
+                last_sighting.write().await.replace(sighting);
+            }
+            sleep(CV_LOOP_SLEEP_TIME).await;
+        }
+    });
+}
+
+
+pub struct Image {}
+
+pub struct CVSubsystem {}
+
+impl CVSubsystem {
+    pub fn new() -> Self {
+        Self {}
+    }
+    pub fn get_dog(&self ) -> Option<Image> {
+        Some(Image {})
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,30 @@
+use std::alloc::System;
+use std::time::Duration;
 use std::{time::SystemTime, sync::Arc};
 
-use mock_cv::Image;
-use mock_cv::CVSubsystem;
+use cv::Image;
+use cv::setup_cv_loop;
 
 use tokio::sync::Mutex;
+use tokio::sync::broadcast::Receiver;
 use tokio::sync::RwLock;
+use tokio::time::sleep;
 
 
-mod mock_cv;
+mod cv;
 
 #[tokio::main]
 async fn main() {
     println!("Hello, world!");
 
-    let cv_subsystem = CVSubsystem::new();
 
-    let last_dog_sighting: Arc<RwLock<Option<DogSighting>>> = Arc::new(RwLock::new(None));
+    let last_sighting: Arc<RwLock<Option<DogSighting>>> = Arc::new(RwLock::new(None));
 
-    {
-        let last_dog_sighting = last_dog_sighting.clone();
-        tokio::spawn(async move {
-            loop {
-                if let Some(sighting) = cv_subsystem.get_dog() {
-                    s = last_dog_sighting.write().await;
-                }
-            }
-        });
-    }
+    setup_cv_loop(last_sighting);
+
+    // TODO web server stuff
 }
+
 
 pub struct DogSighting {
     timestamp: SystemTime,
